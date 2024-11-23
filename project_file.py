9b0bd432ebdc52c4659d8e0e2c745b3c46a6e29a
NikitOK11 <nlkltok2024@gmail.com>
2024-11-23 10:55:02 +0300

    Moved finger index finger detection inside of a function

--- project_file.py
+++ project_file.py
@@ -3,6 +3,37 @@ import mediapipe as mp
 import numpy as np
 
 
+def check_only_finger(image):
+    hands = handsDetector.process(image)
+
+    if hands.multi_hand_landmarks is not None:
+        y_thumb_tip = int(hands.multi_hand_landmarks[0].landmark[4].y * image.shape[0])
+        y_thumb_mcp = int(hands.multi_hand_landmarks[0].landmark[2].y * image.shape[0])
+        x_thumb_tip = int(hands.multi_hand_landmarks[0].landmark[4].x * image.shape[1])
+        x_thumb_mcp = int(hands.multi_hand_landmarks[0].landmark[2].x * image.shape[1])
+
+        y_index_tip = int(hands.multi_hand_landmarks[0].landmark[8].y * image.shape[0])
+        y_index_mcp = int(hands.multi_hand_landmarks[0].landmark[5].y * image.shape[0])
+
+        y_middle_finger_tip = int(hands.multi_hand_landmarks[0].landmark[12].y * image.shape[0])
+        y_middle_finger_mcp = int(hands.multi_hand_landmarks[0].landmark[9].y * image.shape[0])
+
+        y_ring_finger_tip = int(hands.multi_hand_landmarks[0].landmark[16].y * image.shape[0])
+        y_ring_finger_mcp = int(hands.multi_hand_landmarks[0].landmark[13].y * image.shape[0])
+
+        y_pinky_tip = int(hands.multi_hand_landmarks[0].landmark[20].y * image.shape[0])
+        y_pinky_mcp = int(hands.multi_hand_landmarks[0].landmark[17].y * image.shape[0])
+
+        if abs(y_thumb_tip - y_thumb_mcp) <= 110 and abs(y_middle_finger_tip - y_middle_finger_mcp) <= 110 and abs(
+                y_ring_finger_tip - y_ring_finger_mcp) <= 110 and abs(
+                y_pinky_tip - y_pinky_mcp) <= 110 and y_index_tip <= y_index_mcp and abs(
+                x_thumb_tip - x_thumb_mcp) <= 70:
+            return True
+        return False
+
+    return False
+
+
 with mp.solutions.hands.Hands(static_image_mode=True, max_num_hands=1, min_detection_confidence=0.5) as handsDetector:
     capture = cv2.VideoCapture(0)
     while capture.isOpened():
@@ -10,42 +41,42 @@ with mp.solutions.hands.Hands(static_image_mode=True, max_num_hands=1, min_detec
         if cv2.waitKey(1) & 0xFF == ord('q') or not ret:
             break
 
-        only_index_finger = False
-
         flipped = np.fliplr(frame)
         flippedRGB = cv2.cvtColor(flipped, cv2.COLOR_BGR2RGB)
 
-        hands = handsDetector.process(flippedRGB)
-        if hands.multi_hand_landmarks is not None:
-            x_thumb_tip = int(hands.multi_hand_landmarks[0].landmark[4].y * flipped.shape[0])
-            x_thumb_mcp = int(hands.multi_hand_landmarks[0].landmark[2].y * flipped.shape[0])
-
-            x_index_tip = int(hands.multi_hand_landmarks[0].landmark[8].y * flipped.shape[0])
-            x_index_mcp = int(hands.multi_hand_landmarks[0].landmark[5].y * flipped.shape[0])
-
-            x_middle_finger_tip = int(hands.multi_hand_landmarks[0].landmark[12].y * flipped.shape[0])
-            x_middle_finger_mcp = int(hands.multi_hand_landmarks[0].landmark[9].y * flipped.shape[0])
-
-            x_ring_finger_tip = int(hands.multi_hand_landmarks[0].landmark[16].y * flipped.shape[0])
-            x_ring_finger_mcp = int(hands.multi_hand_landmarks[0].landmark[13].y * flipped.shape[0])
-
-            x_pinky_tip = int(hands.multi_hand_landmarks[0].landmark[20].y * flipped.shape[0])
-            x_pinky_mcp = int(hands.multi_hand_landmarks[0].landmark[17].y * flipped.shape[0])
-
-            print(f"Thumb tip: {x_thumb_tip}, thumb mcp: {x_thumb_mcp}")
-            print(f"index tip: {x_index_tip}, index mcp: {x_index_mcp}")
-            print(f"middle tip: {x_middle_finger_tip}, middle mcp: {x_middle_finger_mcp}")
-            print(f"ring tip: {x_ring_finger_tip}, ring mcp: {x_ring_finger_mcp}")
-            print(f"pinky tip: {x_index_tip}, pinky mcp: {x_index_mcp}")
-            print("")
-
-            if abs(x_thumb_tip - x_thumb_mcp) <= 110 and abs(x_middle_finger_tip - x_middle_finger_mcp) <= 110 and abs(x_ring_finger_tip - x_ring_finger_mcp) <= 110 and abs(x_pinky_tip - x_pinky_mcp) <= 110 and x_index_tip <= x_index_mcp:
-                only_index_finger = True
-
-            if only_index_finger:
-                cv2.circle(flippedRGB, (flipped.shape[0] // 2, flipped.shape[1] // 2), 10, (0, 255, 0), -1)
-            else:
-                cv2.circle(flippedRGB, (flipped.shape[0] // 2, flipped.shape[1] // 2), 10, (255, 0, 0), -1)
+        # hands = handsDetector.process(flippedRGB)
+        # if hands.multi_hand_landmarks is not None:
+        #     y_thumb_tip = int(hands.multi_hand_landmarks[0].landmark[4].y * flipped.shape[0])
+        #     y_thumb_mcp = int(hands.multi_hand_landmarks[0].landmark[2].y * flipped.shape[0])
+        #     x_thumb_tip = int(hands.multi_hand_landmarks[0].landmark[4].x * flipped.shape[1])
+        #     x_thumb_mcp = int(hands.multi_hand_landmarks[0].landmark[2].x * flipped.shape[1])
+        #
+        #     y_index_tip = int(hands.multi_hand_landmarks[0].landmark[8].y * flipped.shape[0])
+        #     y_index_mcp = int(hands.multi_hand_landmarks[0].landmark[5].y * flipped.shape[0])
+        #
+        #     y_middle_finger_tip = int(hands.multi_hand_landmarks[0].landmark[12].y * flipped.shape[0])
+        #     y_middle_finger_mcp = int(hands.multi_hand_landmarks[0].landmark[9].y * flipped.shape[0])
+        #
+        #     y_ring_finger_tip = int(hands.multi_hand_landmarks[0].landmark[16].y * flipped.shape[0])
+        #     y_ring_finger_mcp = int(hands.multi_hand_landmarks[0].landmark[13].y * flipped.shape[0])
+        #
+        #     y_pinky_tip = int(hands.multi_hand_landmarks[0].landmark[20].y * flipped.shape[0])
+        #     y_pinky_mcp = int(hands.multi_hand_landmarks[0].landmark[17].y * flipped.shape[0])
+        #
+        #     print(f"Thumb tip: {y_thumb_tip}, thumb mcp: {y_thumb_mcp}")
+        #     print(f"index tip: {y_index_tip}, index mcp: {y_index_mcp}")
+        #     print(f"middle tip: {y_middle_finger_tip}, middle mcp: {y_middle_finger_mcp}")
+        #     print(f"ring tip: {y_ring_finger_tip}, ring mcp: {y_ring_finger_mcp}")
+        #     print(f"pinky tip: {y_index_tip}, pinky mcp: {y_index_mcp}")
+        #     print("")
+        #
+        #     if abs(y_thumb_tip - y_thumb_mcp) <= 110 and abs(y_middle_finger_tip - y_middle_finger_mcp) <= 110 and abs(y_ring_finger_tip - y_ring_finger_mcp) <= 110 and abs(y_pinky_tip - y_pinky_mcp) <= 110 and y_index_tip <= y_index_mcp and abs(x_thumb_tip - x_thumb_mcp) <= 70:
+        #         only_index_finger = True
+        only_index_finger = check_only_finger(flippedRGB)
+        if only_index_finger:
+            cv2.circle(flippedRGB, (flipped.shape[0] // 2, flipped.shape[1] // 2), 10, (0, 255, 0), -1)
+        else:
+            cv2.circle(flippedRGB, (flipped.shape[0] // 2, flipped.shape[1] // 2), 10, (255, 0, 0), -1)
 
         res_image = cv2.cvtColor(flippedRGB, cv2.COLOR_RGB2BGR)
         cv2.imshow("Hands", res_image)
