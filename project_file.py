b6c3289766e96df4cdaf401bd855b00bd3f190a4
NikitOK11 <nlkltok2024@gmail.com>
2024-12-06 20:26:25 +0300

    Renamed variables

--- project_file.py
+++ project_file.py
@@ -6,26 +6,26 @@ import Settings
 import os
 
 
-def check_only_finger(image) -> bool:
-    hands = handsDetector.process(image)
+def check_only_finger(frame) -> bool:
+    hands = handsDetector.process(frame)
 
     if hands.multi_hand_landmarks is not None:
-        y_thumb_tip = int(hands.multi_hand_landmarks[0].landmark[4].y * image.shape[0])
-        y_thumb_mcp = int(hands.multi_hand_landmarks[0].landmark[2].y * image.shape[0])
-        x_thumb_tip = int(hands.multi_hand_landmarks[0].landmark[4].x * image.shape[1])
-        x_thumb_mcp = int(hands.multi_hand_landmarks[0].landmark[2].x * image.shape[1])
+        y_thumb_tip = int(hands.multi_hand_landmarks[0].landmark[4].y * frame.shape[0])
+        y_thumb_mcp = int(hands.multi_hand_landmarks[0].landmark[2].y * frame.shape[0])
+        x_thumb_tip = int(hands.multi_hand_landmarks[0].landmark[4].x * frame.shape[1])
+        x_thumb_mcp = int(hands.multi_hand_landmarks[0].landmark[2].x * frame.shape[1])
 
-        y_index_tip = int(hands.multi_hand_landmarks[0].landmark[8].y * image.shape[0])
-        y_index_mcp = int(hands.multi_hand_landmarks[0].landmark[5].y * image.shape[0])
+        y_index_tip = int(hands.multi_hand_landmarks[0].landmark[8].y * frame.shape[0])
+        y_index_mcp = int(hands.multi_hand_landmarks[0].landmark[5].y * frame.shape[0])
 
-        y_middle_finger_tip = int(hands.multi_hand_landmarks[0].landmark[12].y * image.shape[0])
-        y_middle_finger_mcp = int(hands.multi_hand_landmarks[0].landmark[9].y * image.shape[0])
+        y_middle_finger_tip = int(hands.multi_hand_landmarks[0].landmark[12].y * frame.shape[0])
+        y_middle_finger_mcp = int(hands.multi_hand_landmarks[0].landmark[9].y * frame.shape[0])
 
-        y_ring_finger_tip = int(hands.multi_hand_landmarks[0].landmark[16].y * image.shape[0])
-        y_ring_finger_mcp = int(hands.multi_hand_landmarks[0].landmark[13].y * image.shape[0])
+        y_ring_finger_tip = int(hands.multi_hand_landmarks[0].landmark[16].y * frame.shape[0])
+        y_ring_finger_mcp = int(hands.multi_hand_landmarks[0].landmark[13].y * frame.shape[0])
 
-        y_pinky_tip = int(hands.multi_hand_landmarks[0].landmark[20].y * image.shape[0])
-        y_pinky_mcp = int(hands.multi_hand_landmarks[0].landmark[17].y * image.shape[0])
+        y_pinky_tip = int(hands.multi_hand_landmarks[0].landmark[20].y * frame.shape[0])
+        y_pinky_mcp = int(hands.multi_hand_landmarks[0].landmark[17].y * frame.shape[0])
 
         if abs(y_thumb_tip - y_thumb_mcp) <= 110 and abs(y_middle_finger_tip - y_middle_finger_mcp) <= 110 and abs(
                 y_ring_finger_tip - y_ring_finger_mcp) <= 110 and abs(
@@ -34,6 +34,13 @@ def check_only_finger(image) -> bool:
             return True
         return False
     return False
+
+
+def check_hands(frame):
+    hands = handsDetector.process(frame)
+    if hands.multi_hand_landmarks is None:
+        return False
+    return True
 
 
 def load_best_score():
@@ -68,10 +75,10 @@ def reset_settings() -> None:
 
 
 # noinspection PyGlobalUndefined
-def phaseCapturingFinger(frm) -> None:
-    cv2.circle(frm, (frm.shape[1] // 2, frm.shape[0] // 2), 5, (44, 62, 80), -1)
+def phaseCapturingFinger(frame) -> None:
+    cv2.circle(frame, (frame.shape[1] // 2, frame.shape[0] // 2), 5, (44, 62, 80), -1)
     if Settings.only_index_finger:
-        cv2.putText(frm, "Captured your finger!", (160, 20),
+        cv2.putText(frame, "Captured your finger!", (160, 20),
                     cv2.FONT_HERSHEY_SIMPLEX,
                     0.8, (0, 0, 0))
         if Settings.START_PHASE_TWO == 25:
@@ -80,19 +87,19 @@ def phaseCapturingFinger(frm) -> None:
             Settings.START_PHASE_TWO += 1
     else:
         Settings.only_index_finger = check_only_finger(flippedRGB)
-        cv2.putText(flippedRGB, "Can't see your index finger...", (120, 30),
-                    cv2.FONT_HERSHEY_SIMPLEX, 0.9, (0, 0, 0))
+        cv2.putText(flippedRGB, "Can't see your index finger...", (100, 45),
+                    cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 0))
 
 
 # noinspection PyGlobalUndefined
-def phaseDrawingCircle(frm) -> None:
-    hands = handsDetector.process(frm)
+def phaseDrawingCircle(frame) -> None:
+    hands = handsDetector.process(frame)
 
-    cv2.circle(frm, (frm.shape[1] // 2, frm.shape[0] // 2), 5, (44, 62, 80), -1)
+    cv2.circle(frame, (frame.shape[1] // 2, frame.shape[0] // 2), 5, (44, 62, 80), -1)
     if not Settings.DRAWING:
         cv2.putText(flippedRGB, "Place your finger to the point from which you start!", (15, 20),
                     cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 0))
-        cv2.putText(frm, f"Start drawing in {Settings.SECONDS_UNTIL_DRAWING} seconds", (140, 50),
+        cv2.putText(frame, f"Start drawing in {Settings.SECONDS_UNTIL_DRAWING} seconds", (140, 50),
                     cv2.FONT_HERSHEY_SIMPLEX,
                     0.8, (0, 0, 0))
 
@@ -104,21 +111,21 @@ def phaseDrawingCircle(frm) -> None:
 
         if Settings.SECONDS_UNTIL_DRAWING == 0:
             if hands.multi_hand_landmarks is not None:
-                x_index_tip = int(hands.multi_hand_landmarks[0].landmark[8].x * frm.shape[1])
-                y_index_tip = int(hands.multi_hand_landmarks[0].landmark[8].y * frm.shape[0])
-                Settings.circle_radius = math.hypot(abs(frm.shape[1] // 2 - x_index_tip), abs(frm.shape[0] // 2 - y_index_tip))
+                x_index_tip = int(hands.multi_hand_landmarks[0].landmark[8].x * frame.shape[1])
+                y_index_tip = int(hands.multi_hand_landmarks[0].landmark[8].y * frame.shape[0])
+                Settings.circle_radius = math.hypot(abs(frame.shape[1] // 2 - x_index_tip), abs(frame.shape[0] // 2 - y_index_tip))
 
                 Settings.index_frame_circles.append((x_index_tip, y_index_tip))
             Settings.DRAWING = True
     else:
-        x_index_tip = int(hands.multi_hand_landmarks[0].landmark[8].x * frm.shape[1])
-        y_index_tip = int(hands.multi_hand_landmarks[0].landmark[8].y * frm.shape[0])
-        if math.hypot(x_index_tip, y_index_tip, frm.shape[1] // 2, frm.shape[0] // 2) < 10:
-            cv2.putText(frm, f"Your finger is too close to center!", (105, 20),
+        x_index_tip = int(hands.multi_hand_landmarks[0].landmark[8].x * frame.shape[1])
+        y_index_tip = int(hands.multi_hand_landmarks[0].landmark[8].y * frame.shape[0])
+        if math.hypot(x_index_tip, y_index_tip, frame.shape[0] // 2, frame.shape[1] // 2) < 300:
+            cv2.putText(frame, f"Your finger is too close to center!", (105, 20),
                         cv2.FONT_HERSHEY_SIMPLEX,
                         0.8, (0, 0, 0))
         else:
-            cv2.putText(frm, f"Draw the circle around the dot", (105, 20),
+            cv2.putText(frame, f"Draw the circle around the dot", (105, 20),
                         cv2.FONT_HERSHEY_SIMPLEX,
                         0.8, (0, 0, 0))
 
@@ -127,7 +134,7 @@ def phaseDrawingCircle(frm) -> None:
                 circle_point = Settings.index_frame_circles[i]
                 x_tip, y_tip = circle_point[0], circle_point[1]
 
-                dist_from_center = math.hypot(abs(frm.shape[1] // 2 - x_tip), abs(frm.shape[0] // 2 - y_tip))
+                dist_from_center = math.hypot(abs(frame.shape[1] // 2 - x_tip), abs(frame.shape[0] // 2 - y_tip))
                 mistake = abs(Settings.circle_radius - dist_from_center) // 4
 
                 difference = 1 - (abs(Settings.circle_radius - dist_from_center) / Settings.circle_radius) * 2.5
@@ -136,24 +143,24 @@ def phaseDrawingCircle(frm) -> None:
 
                 color_mistake = Settings.colors_error_from_radius[int(min(5, mistake))]
                 if i > 0:
-                    cv2.line(frm, (x_tip, y_tip),
+                    cv2.line(frame, (x_tip, y_tip),
                              (Settings.index_frame_circles[i - 1][0],
                               Settings.index_frame_circles[i - 1][1]), color_mistake, 4)
 
             if len(Settings.index_frame_circles) > 0:
                 Settings.ACCURACY_DRAWING = accuracy_sum / len(Settings.index_frame_circles)
 
-            cv2.putText(frm, f"Accuracy: {round(Settings.ACCURACY_DRAWING * 100, 1)}%",
+            cv2.putText(frame, f"Accuracy: {round(Settings.ACCURACY_DRAWING * 100, 1)}%",
                         (230, 40), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0, 0, 0))
 
             if hands.multi_hand_landmarks is not None:
-                x_index_tip = int(hands.multi_hand_landmarks[0].landmark[8].x * frm.shape[1])
-                y_index_tip = int(hands.multi_hand_landmarks[0].landmark[8].y * frm.shape[0])
+                x_index_tip = int(hands.multi_hand_landmarks[0].landmark[8].x * frame.shape[1])
+                y_index_tip = int(hands.multi_hand_landmarks[0].landmark[8].y * frame.shape[0])
                 Settings.index_frame_circles.append((x_index_tip, y_index_tip))
 
                 if hands.multi_hand_landmarks is not None:
-                    x_index_tip = int(hands.multi_hand_landmarks[0].landmark[8].x * frm.shape[1])
-                    y_index_tip = int(hands.multi_hand_landmarks[0].landmark[8].y * frm.shape[0])
+                    x_index_tip = int(hands.multi_hand_landmarks[0].landmark[8].x * frame.shape[1])
+                    y_index_tip = int(hands.multi_hand_landmarks[0].landmark[8].y * frame.shape[0])
                     Settings.index_frame_circles.append((x_index_tip, y_index_tip))
 
                     if len(Settings.index_frame_circles) > 150:
@@ -170,33 +177,33 @@ def phaseDrawingCircle(frm) -> None:
                             Settings.CURRENT_PHASE = phaseEndGame
 
 
-def phaseEndGame(frm) -> None:
-    cv2.putText(frm, f"Accuracy: {round(Settings.ACCURACY_DRAWING * 100, 1)}%",
+def phaseEndGame(frame) -> None:
+    cv2.putText(frame, f"Accuracy: {round(Settings.ACCURACY_DRAWING * 100, 1)}%",
                 (230, 40), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0, 0, 0))
 
     best_score = load_best_score()
     if Settings.ACCURACY_DRAWING > best_score:
         save_best_score(Settings.ACCURACY_DRAWING)
-        cv2.putText(frm, "Congratulations! New Record!", (140, 100),
+        cv2.putText(frame, "Congratulations! New Record!", (140, 100),
                     cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0, 0, 255))
 
     for i in range(len(Settings.index_frame_circles)):
         circle_point = Settings.index_frame_circles[i]
         x_tip, y_tip = circle_point[0], circle_point[1]
 
-        dist_from_center = math.hypot(abs(frm.shape[1] // 2 - x_tip), abs(frm.shape[0] // 2 - y_tip))
+        dist_from_center = math.hypot(abs(frame.shape[1] // 2 - x_tip), abs(frame.shape[0] // 2 - y_tip))
         mistake = abs(Settings.circle_radius - dist_from_center) // 4
         color_mistake = Settings.colors_error_from_radius[int(min(5, mistake))]
         if i > 0:
-            cv2.line(frm, (x_tip, y_tip),
+            cv2.line(frame, (x_tip, y_tip),
                      (Settings.index_frame_circles[i - 1][0],
                       Settings.index_frame_circles[i - 1][1]), color_mistake, 4)
         else:
-            cv2.line(frm, (x_tip, y_tip),
+            cv2.line(frame, (x_tip, y_tip),
                      (Settings.index_frame_circles[-1][0],
                       Settings.index_frame_circles[-1][1]), color_mistake, 4)
 
-    cv2.putText(frm, "Press Enter to play again", (180, 75),
+    cv2.putText(frame, "Press Enter to play again", (180, 75),
                 cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0, 0, 255))
 
     if cv2.waitKey(1) & 0xFF == 13:
@@ -208,11 +215,11 @@ Settings.CURRENT_PHASE = phaseCapturingFinger
 with mp.solutions.hands.Hands(static_image_mode=True, max_num_hands=1, min_detection_confidence=0.5) as handsDetector:
     capture = cv2.VideoCapture(0)
     while capture.isOpened():
-        ret, frame = capture.read()
+        ret, frame_recorded = capture.read()
         if not ret:
             break
 
-        flippedRGB = cv2.cvtColor(np.fliplr(frame), cv2.COLOR_BGR2RGB)
+        flippedRGB = cv2.cvtColor(np.fliplr(frame_recorded), cv2.COLOR_BGR2RGB)
         Settings.CURRENT_PHASE(flippedRGB)
         res_image = cv2.cvtColor(flippedRGB, cv2.COLOR_RGB2BGR)
 
